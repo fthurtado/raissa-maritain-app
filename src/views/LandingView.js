b1f6d246334662ee60861ef18d0406b485074871
Nicolás Bravo de la Cruz <nhbravo@uc.cl>
2021-06-17 23:39:15 -0400

    landing

--- src/views/LandingView.js
+++ src/views/LandingView.js
@@ -5,12 +5,33 @@ import '../styles/LandingViewStyles.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-function LandingView() {
+import raissa1 from '../assets/images/raisa-escribiendo.jpeg';
 
+function LandingView() {
   return (
     <div className="App">
       <Navbar />
-      <p>Landing</p>
+      <div style={{ width: '100%', height: 'calc(100vh - 150px - 13vh)' }}>
+        <p className="p">
+          Raissa Maritain
+        </p>
+        <p className="p">
+          TTF039 - El Padrenuestro: La oración que Jesús Enseñó
+        </p>
+        <p className="p">
+          Profesor Rodrígo Álvarez Gutiérrez / Hno. Bernardo Álvarez SBO
+        </p>
+        <div className="carouselContainer">
+          <img
+            src={raissa1}
+            alt="raissa-1"
+            className="image"
+          />
+        </div>
+        <p className="p">
+          Utiliza los botones de la barra de navegación para aventurarte a conocer más sobre una de las pensadoras más importantes del siglo XX.
+        </p>
+      </div>
       <Footer />
     </div>
   );
